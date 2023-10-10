9e454f916ddfb4716ae9977aad62d96af7c47fa4
Wyatt <wbrambl1@students.kennesaw.edu>
2023-10-10 14:34:05 -0400

    .

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,53 @@
-import java.util.Objects;
+
+import java.util.Scanner;
 
 public class Main {
-
+    //Repeats a string a certin amount of times
     public static String repeatNTimes(String s,int i){
         if(i == 1){
-            return  s + "\n";
+            return  s;
         }
         else{
-            return s + "\n" + repeatNTimes(s, i-1);
+            return s + repeatNTimes(s, i-1);
         }
     }
-
+    //Checks if a string is reversed version of other string
     public static Boolean isReverse(String s,String ss){
-        if(Objects.equals(s, ss)){
+        if (s.isEmpty() && ss.isEmpty()) {
             return true;
-        }
-        if(s.isEmpty()){
+        } else if (s.length() != ss.length()) {
             return false;
         }
-        else{
-            return isReverse(s.substring(1) + s.charAt(0), ss);
-
+        if(s.charAt(0) == ss.charAt(ss.length() -1)){
+            return isReverse(s.substring(1), ss.substring(0,ss.length()-1));
         }
+        else if((s.charAt(0) != ss.charAt(ss.length() -1))){
+            return false;
+        }
+        return true;
     }
 
 
 
 
     public static void main(String[] args) {
-        //System.out.println(repeatNTimes("I must study recursion until it makes sense", 100));
-        System.out.println(isReverse("fad","daf"));
+        Scanner x = new Scanner(System.in);
+        String one;
+        String two;
+        System.out.println(repeatNTimes("I must study recursion until it makes sense\n", 100));
+
+        System.out.println("Enter the first string: ");
+        one = x.nextLine();
+
+        System.out.println("Enter the second string: ");
+        two = x.nextLine();
+
+        if(isReverse(one, two)){
+            System.out.println(one + " is the reverse of " + two);
+        }
+        else{
+            System.out.println(one + " is not the reverse of " + two);
+        }
 
     }
 }
